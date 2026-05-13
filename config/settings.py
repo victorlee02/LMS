33db883e09e609b19c058ada1383ac8e5cc993ae
[victoelee02] <[alex9018@hufs.ac.kr]>
2026-05-13 19:27:00 +0900

    feat: DB를 MySQL에서 SQLite로 변경

--- config/settings.py
+++ config/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lms',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
